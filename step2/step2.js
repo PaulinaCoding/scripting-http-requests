c8cd5c0486651eb9bba972c6099552bff8e66934
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-05-15 00:33:43 +0000

    Revided and improved

--- step2/step2.js
+++ step2/step2.js
@@ -6,7 +6,7 @@ var https = require('https');
 // particular resource (like '/login/index.html'), that would be defined in the path
 
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML() {
 
   var requestOptions = {
     host: 'sytantris.github.io',
@@ -35,4 +35,4 @@ function getAndPrintHTMLChunks () {
   });
 
 }
-getAndPrintHTMLChunks()
+getAndPrintHTML()
